440ac71c82762ddd9ea79601bf2cb6e13b5baa29
notlath <qltpagsuguiron@tip.edu.ph>
2024-04-06 16:28:18 +0800

    Refactor Ingredients class to update ingredient lists

--- core/src/com/whatstherecipe/game/classes/Ingredients.java
+++ core/src/com/whatstherecipe/game/classes/Ingredients.java
@@ -2,8 +2,8 @@ package com.whatstherecipe.game.classes;
 
 public class Ingredients {
     public static String[][] ingredientsList = {
-            { "bile" },
-            { "calamansi" },
-            { "chicharon" }
+            { "bile", "chicken", "ginger", "onion", "rice-noodles", "tamarind" }, // 0
+            { "calamansi", "chili", "ketchup", "patis", "shrimps", "tomatoes" }, // 1
+            { "chicharon", "garlic", "mayonnaise", "pork ribs", "sisig-meat", "toyo" } // 2
     };
 }
