b60c291fc5e54a4fee2582821c27c90f648e87c7
Richard William <rwilliam.flores@gmail.com>
2024-04-21 14:01:31 +0800

    Updated UI

--- core/src/com/whatstherecipe/game/components/IngredientSelection.java
+++ core/src/com/whatstherecipe/game/components/IngredientSelection.java
@@ -1,8 +1,12 @@
 package com.whatstherecipe.game.components;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.whatstherecipe.game.WhatsTheRecipe;
 import com.whatstherecipe.game.classes.Meal;
 import com.whatstherecipe.game.ui.Colors;
@@ -10,26 +14,44 @@ import com.whatstherecipe.game.ui.CustomSkin;
 
 import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.math.Interpolation;
 
 public class IngredientSelection {
-    private final WhatsTheRecipe game;
-    private Stage stage;
-    private Meal meal;
+    public final WhatsTheRecipe game;
+    public Stage stage;
+    public Meal meal;
     private Table table;
+    private Table leftTable;
+    private Table centerTable;
+    private Table rightTable;
+    private StepSorting stepSorting;
+    private RecipePaperView recipePaperView;
 
-    public IngredientSelection(final WhatsTheRecipe game, Stage stage, Meal meal) {
-        this.game = game;
-        this.stage = stage;
-        this.meal = meal;
+    public IngredientSelection(RecipePaperView recipePaperView) {
+        this.game = recipePaperView.game;
+        this.stage = recipePaperView.stage;
+        this.meal = recipePaperView.meal;
+        this.stepSorting = recipePaperView.stepSorting;
+        this.recipePaperView = recipePaperView;
 
         initTables();
-        initLabels();
+        renderLeft();
+        renderCenter();
+        renderRight();
     }
 
     private void initTables() {
         this.table = new Table();
+        this.leftTable = new Table();
+        this.centerTable = new Table();
+        this.rightTable = new Table();
+
         this.table.setFillParent(true);
+        this.table.add(leftTable).expand().left().padLeft(100);
+        this.table.add(centerTable).expand().center();
+        this.table.add(rightTable).expand().right().padRight(100);
     }
 
     public void show() {
@@ -45,9 +67,77 @@ public class IngredientSelection {
         })));
     }
 
-    private void initLabels() {
-        Label label = new Label("Ingredient selection", CustomSkin.generateCustomLilitaOneFont(Colors.lightBrown, 32));
+    private void renderLeft() {
+        Label mealName = new Label(meal.name, CustomSkin.generateCustomLilitaOneFont(Colors.lightBrown, 64));
+        ArrayList<Label> instructions = new ArrayList<Label>();
 
-        this.table.add(label).expand().center();
+        this.leftTable.add(mealName).left().padBottom(15).row();
+
+        instructions.add(
+                new Label("Listed here on the paper are the ingredients for cooking " + meal.name
+                        + ". Find and collect them first from the 4 labeled cabinets back in the kitchen to proceed to the next phase: finding out the correct steps for cooking "
+                        + meal.name + ".",
+                        CustomSkin.generateCustomLilitaOneFont(Colors.lightBrown, 32)));
+        instructions.add(new Label(
+                "Use your basket at the bottom right for collecting ingredients, viewing them, as well as removing those that you don't want anymore!",
+                CustomSkin.generateCustomLilitaOneFont(Colors.lightBrown, 32)));
+
+        instructions.forEach(instruction -> {
+            instruction.setWrap(true);
+
+            this.leftTable.add(instruction).width(400).padBottom(15).row();
+        });
+    }
+
+    private void renderCenter() {
+        for (int i = 1; i <= this.meal.ingredients.size(); i++) {
+            String ingredient = this.meal.ingredients.get(i - 1);
+
+            ingredient = ingredient.substring(0, 1).toUpperCase() + ingredient.substring(1);
+
+            Label ingredientLabel = new Label(i + ". " + ingredient,
+                    CustomSkin.generateCustomLilitaOneFont(Colors.brown, 32));
+
+            this.centerTable.add(ingredientLabel).width(300).padBottom(15).row();
+        }
+    }
+
+    private void renderRight() {
+        Table instructionsContainer = new Table();
+        ArrayList<Label> instructions = new ArrayList<Label>();
+
+        instructions.add(new Label("Click on the button below to check if your basket contains the right ingredients!",
+                CustomSkin.generateCustomLilitaOneFont(Colors.lightBrown, 32)));
+        instructions.add(new Label(
+                "Remember: Having the wrong collection of ingredients will cost you a star, and the right collection will give you one star (if you don't already have 3 stars)!",
+                CustomSkin.generateCustomLilitaOneFont(Colors.lightBrown, 32)));
+
+        instructions.forEach(instruction -> {
+            instruction.setWrap(true);
+
+            instructionsContainer.add(instruction).width(400).padBottom(15).row();
+        });
+
+        this.rightTable.add(instructionsContainer).row();
+
+        TextButton checkBtn = new TextButton(
+                "Check ingredients",
+                this.game.skin.get("text-button-default", TextButtonStyle.class));
+
+        checkBtn.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                hide();
+
+                recipePaperView.phase = "step-sorting";
+
+                stepSorting.show();
+
+                return true;
+            }
+
+        });
+
+        this.rightTable.add(checkBtn).padTop(50);
     }
 }
