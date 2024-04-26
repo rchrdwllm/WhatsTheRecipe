f076d21020c408313cfc6310c34d1f95c88375fb
Richard William <rwilliam.flores@gmail.com>
2024-04-26 16:58:01 +0800

    Updated ingredients cabinet index

--- core/src/com/whatstherecipe/game/classes/Ingredients.java
+++ core/src/com/whatstherecipe/game/classes/Ingredients.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public class Ingredients {
     public static String[][] ingredientsList = {
-            { "bile", "suka", "ketchup", "chicharon", "tomatoes" }, // 0
+            { "bile", "garlic", "onion", "chicharon", "tomatoes" }, // 0
             { "calamansi", "chili", "ginger", "tamarind", "shrimps", "peppercorn" }, // 1
-            { "onion", "garlic", "mayonnaise", "patis", "toyo" }, // 2
+            { "ketchup", "suka", "mayonnaise", "patis", "toyo" }, // 2
             { "rice-noodles", "pork-ribs", "sisig-meat", "chicken", "goat-meat" } // 3
     };
 
