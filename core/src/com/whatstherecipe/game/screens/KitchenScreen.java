f5937fbbc7e5f74d8054e96568b2e9cf847561d7
Richard William <rwilliam.flores@gmail.com>
2024-04-27 16:26:16 +0800

    Removed unused import

--- core/src/com/whatstherecipe/game/screens/KitchenScreen.java
+++ core/src/com/whatstherecipe/game/screens/KitchenScreen.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
